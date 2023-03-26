9796cbab39a4c340580ba7997aed4d28d577e061
Viransh Shah <shahv47@mcmaster.ca>
2023-03-26 18:22:44 -0400

    3 row

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div class="overflow-y:auto">
       <Navbar/>
       {/* <ImageGallery/> */}
       <Positions/>
