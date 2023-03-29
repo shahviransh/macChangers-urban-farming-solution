3d82a5eb4fb7edb55312dfd9026f9594d4d3d219
Viransh Shah <shahv47@mcmaster.ca>
2023-03-29 16:27:52 -0400

    moved to public

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import ImageGallery from './components/ImageGallery'
