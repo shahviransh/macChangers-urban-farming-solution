d8334557f249b9f7bbf974e46b7217c23028b0c0
asad-mhm <96676879+asad-mhm@users.noreply.github.com>
2023-03-29 15:19:16 -0400

    updated positions

--- src/components/Positions.jsx
+++ src/components/Positions.jsx
@@ -8,7 +8,8 @@ const Positions = () => {
       <div className='Pos'>
         <h1 className='Pos__title'>Positions</h1>
         <div className='Pos__container'>
-          <PositionCard Position="Gardener" Company="UrbanFarmsOrg" Description="VOLUNTEERING POSITION! 1" link="https://timothy-leung.vercel.app/" />
+          <PositionCard Position="Plant Interrogator" Company="By Hamilton Planters" Description="Help needed handling intrusive plant species situated around the garden.
+Location: Hamilton" link="https://timothy-leung.vercel.app/" />
           <PositionCard Position="Gardener" Company="UrbanFarmsOrg" Description="VOLUNTEERING POSITION! 2" link="https://timothy-leung.vercel.app/" />
           <PositionCard Position="Gardener" Company="UrbanFarmsOrg" Description="VOLUNTEERING POSITION! 3" link="https://timothy-leung.vercel.app/" />
           <PositionCard Position="Gardener" Company="UrbanFarmsOrg" Description="VOLUNTEERING POSITION! 4" link="https://timothy-leung.vercel.app/" />
