a6acf818e87c279f7256be11a18291b831fb52a6
leungt30 <leungt30@mcmaster.ca>
2023-03-30 16:13:33 -0400

    changed most buttons to white

--- src/components/filterbar.jsx
+++ src/components/filterbar.jsx
@@ -5,35 +5,35 @@ const filterbar = () => {
         <div className='flex justify-between items-center mb-5 px-14 py-2'>
             <div className='date-filter'>
                   <h6 className="text-black"> Start date: </h6>
-                <input type="date" name="date" />
+                <input className="bg-white text-black px-2" type="date" name="date" />
             </div>
             <div className='location-filter'>
                   <h6 className="text-black"> Location: </h6>
-                <input type="text" name="location" />
+                <input className="bg-white text-black px-2" type="text" name="location" />
             </div>
             <div className='type-filter'>
                   <h6 className="text-black"> Type: </h6>
-                <select name="type">
-                    <option value="volunteer">volunteer</option>
-                    <option value="paid">paid</option>
-                    <option value="internship">internship</option>
-                    <option value="coop">coop</option>
+                <select className="bg-white text-black px-2" name="type">
+                    <option  className="bg-white text-black px-2" value="volunteer">volunteer</option>
+                    <option className="bg-white text-black px-2" value="paid">paid</option>
+                    <option className="bg-white text-black px-2" value="internship">internship</option>
+                    <option className="bg-white text-black px-2" value="coop">coop</option>
                 </select>
             </div>
             <div className='searchbar'>
                   <h6 className="text-black">
                     Search: 
                 </h6>
-                <input type="text" name="search" />
+                <input className="bg-white text-black px-2" type="text" name="search" />
             </div>
             <div>
                 <div className='filterbar__checkbox'>
-                    <input type="checkbox" name="checkbox" />
+                    <input className="bg-white" type="checkbox" name="checkbox" />
                       <label className="text-black"> Show only positions with openings</label>
                 </div>
             </div>
             <div>
-                  <button>Search</button>
+                  <button className="bg-white text-black px-2" >Search</button>
             </div>
         </div>
     </div>
