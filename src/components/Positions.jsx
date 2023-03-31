90ee9654950461db919c73d4ce2c6488d7f1f418
asad-mhm <96676879+asad-mhm@users.noreply.github.com>
2023-03-30 22:59:42 -0400

    Update Positions.jsx

--- src/components/Positions.jsx
+++ src/components/Positions.jsx
@@ -10,22 +10,22 @@ const Positions = () => {
         <Filterbar/>
         <div className='Pos__container'>
           <PositionCard Position="Plant Interrogator" Company="By Hamilton Planters" Description="Help needed handling intrusive plant species situated around the garden.
-Location: Hamilton
+| Location: Hamilton | 
 Contact Info: HamiltonPlanters@gmail.com" link="https://timothy-leung.vercel.app/" />
           <PositionCard Position="Irrigation Assistant" Company="Berry Garries" Description="Assistance required managing the plant watering and irrigation system.
-Location: Hamilton
+| Location: Hamilton |
 Contact Info: GarriesCo@hotmail.com" link="https://timothy-leung.vercel.app/" />
           <PositionCard Position="Harvestor" Company="Green Beans" Description="Helping hands welcome to assist in the harvesting process at our farm!
-Location: Hamilton
+| Location: Hamilton |
 Contact Info: gBeans@gmail.com" link="https://timothy-leung.vercel.app/" />
           <PositionCard Position="Harvestor" Company="Hamilton Planters" Description="Helping hands welcome to assist in the harvesting process at our farm!
-Location: Hamilton
+| Location: Hamilton |
 Contact Info: HamiltonPlanters@gmail.com" link="https://timothy-leung.vercel.app/" />
           <PositionCard Position="Distribution Assistant" Company="Green Beans" Description=" Looking for people to assist in the transportation and distribution of crop yields.
-Location: Hamilton
+| Location: Hamilton |
 Contact Info: gBeans@gmail.com" link="https://timothy-leung.vercel.app/" />
           <PositionCard Position="Pruner" Company="Green Beans" Description=" Help needed with managing and pruning the crops of the Green Bean garden.
-Location: Hamilton
+| Location: Hamilton |
 Contact Info: gBeans@gmail.com" link="https://timothy-leung.vercel.app/" />
         </div>
       </div>
